384154cd5b165e59f9b5b59a767ceb34fffb4714
tanbir11 <tanbirul15-3940@diu.edu.bd>
2026-02-17 00:02:47 +0600

    1

--- project.js
+++ project.js
@@ -1,5 +1,9 @@
 console.log("How are you?")
 const value = document.getElementsByTagName('li')
+const ul_tag = document.getElementsByTagName('ul')[0]
+ul_tag.style.borderRadius = '10px'
+ul_tag.style.backgroundColor = 'red'
 for (const v of value) {
-    v.style.backgroundColor = 'red'
+
+    v.style.padding = '10px'
 }
